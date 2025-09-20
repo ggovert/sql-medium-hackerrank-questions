2c85413923deaf1a3f69dd69638b1f896842ef49
Giovanni <111510965+ggovert@users.noreply.github.com>
2025-09-20 12:03:09 -0500

    Question 2

---
+++ weatherObservation.sql
@@ -0,0 +1,10 @@
+with point AS
+    (select min(lat_n) as a,
+            min(long_w) as b,
+            max(lat_n) as c,
+            max(long_w) as d
+    from station)
+
+
+select round(abs(a-c) + abs(b-d), 4)
+from point
